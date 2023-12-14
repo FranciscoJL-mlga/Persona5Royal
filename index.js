3bf60f849f38a9978e1abfb10da51f27799055ee
Francisco Jiménez López <118803641+FranciscoJL-mlga@users.noreply.github.com>
2023-12-14 10:30:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += "responsive";
+    x.className += " responsive";
+   
   } else {
     x.className = "topnav";
   }
@@ -16,7 +17,7 @@ function myFunction() {
 
 
 
-/************************** */
+/************slider************** */
 
 let slideIndex = 1;
 showSlides(slideIndex);
